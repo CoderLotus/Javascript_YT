56398c697ec567d3ea490ed075b0b002c0b3ad25
CoderLotus <abhinavt1704@yahoo.com>
2025-06-01 01:20:00 +0530

    Update 03_arrow.js

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -13,11 +13,11 @@ const user = {
 // user.username = "sam" // Sam is changed here so current context changed so in the next line message is printed for Sam
 // user.welcomeMessage()
 
-// console.log(this);
+// console.log(this); // This prints the content of the current context in global frame here which is none
 
 // function chai(){
 //     let username = "hitesh"
-//     console.log(this.username);
+//     console.log(this.username); //For this.username it does not print "hitesh" as it is a function not an object
 // }
 
 // chai()
@@ -27,28 +27,36 @@ const user = {
 //     console.log(this.username);
 // }
 
+//ARROW FUNCTION : 
 const chai =  () => {
     let username = "hitesh"
-    console.log(this);
+    console.log(this); 
 }
 
 
 // chai()
 
+
+//ARROW FUNCTION :
+
+//Basic arrow function: For curly braces write return keyword 
+
 // const addTwo = (num1, num2) => {
 //     return num1 + num2
 // }
 
+//Implicit Return : It assumes return itself
+
 // const addTwo = (num1, num2) =>  num1 + num2
+// const addTwo = (num1, num2) => ( num1 + num2 ) //For square braces no need to write return keyword
 
-// const addTwo = (num1, num2) => ( num1 + num2 )
-
-const addTwo = (num1, num2) => ({username: "hitesh"})
+const addTwo = (num1, num2) => ({username: "hitesh"}) // To return object
 
 
 console.log(addTwo(3, 4))
 
 
+//More discussion on loops video
 // const myArray = [2, 5, 3, 7, 8]
 
 // myArray.forEach()

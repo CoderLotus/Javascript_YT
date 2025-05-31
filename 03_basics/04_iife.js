65d45918c357792f2455e453d0cb1f296fe1e182
CoderLotus <abhinavt1704@yahoo.com>
2025-06-01 01:25:11 +0530

    Update 04_iife.js

--- 03_basics/04_iife.js
+++ 03_basics/04_iife.js
@@ -1,10 +1,11 @@
-// Immediately Invoked Function Expressions (IIFE)
+// Immediately Invoked Function Expressions (IIFE) => To avoid global scope variables to interfere. Also function executes immediately
 
-
+//First parentheis for function and second parenthesis for execution call
+// () ()
 (function chai(){
     // named IIFE
     console.log(`DB CONNECTED`);
-})();
+})(); //Semicolon important to stop its execution at a point
 
 ( (name) => {
     console.log(`DB CONNECTED TWO ${name}`);

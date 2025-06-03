3446d87de54c4993efe57cc0f93820d10ac7dea3
CoderLotus <abhinavt1704@yahoo.com>
2025-06-03 11:49:19 +0530

    Update four.js

--- 05_iterations/four.js
+++ 05_iterations/four.js
@@ -5,11 +5,16 @@ const myObject = {
     swift: "swift by apple"
 }
 
+//For in loop
+//One use case of for in loop is to iterate an object
+
 for (const key in myObject) {
     //console.log(`${key} shortcut is for ${myObject[key]}`);
 }
 
 const programming = ["js", "rb", "py", "java", "cpp"]
+
+//If we print keys directly here then it prints 0,1,2,3,4 not the values so we have to print this way.
 
 for (const key in programming) {
     //console.log(programming[key]);
@@ -20,6 +25,8 @@ for (const key in programming) {
 // map.set('USA', "United States of America")
 // map.set('Fr', "France")
 // map.set('IN', "India")
+
+//Map is not iteratable so we can't print it this way.
 
 // for (const key in map) {
 //     console.log(key);

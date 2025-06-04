520ea22f736e3abe981c2c5e0f668310ca6e5807
CoderLotus <abhinavt1704@yahoo.com>
2025-06-04 14:24:06 +0530

    Update seven.js

--- 05_iterations/seven.js
+++ 05_iterations/seven.js
@@ -1,6 +1,10 @@
 const myNumers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+//Map automatically returns a value 
+//Whenever you open scope through {} in arrow function always return it through return keyword
 
 // const newNums = myNumers.map( (num) => { return num + 10})
+
+//Chaining => applying 2 or more methods at a time like map then map then filter 
 
 const newNums = myNumers
                 .map((num) => num * 10 )
